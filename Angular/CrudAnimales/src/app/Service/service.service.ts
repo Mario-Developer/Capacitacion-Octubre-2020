2d472f7586e1b212d887259f7547c6e144ce319e
Mario <mariosttone@gmail.com>
2020-11-12 11:36:33 -0600

    n

--- Angular/CrudAnimales/src/app/Service/service.service.ts
+++ Angular/CrudAnimales/src/app/Service/service.service.ts
@@ -8,8 +8,12 @@ export class ServiceService {
   animal: Animal
   constructor(private http:HttpClient) { }
   UrlListar = 'http://localhost:9002/CrudAngularAnimal/crudAngularAnimal/Animal/mostrar';
+  UrlGuardar = 'http://localhost:9002/CrudAngularAnimal/crudAngularAnimal/Animal/guardar';
 
   listarAnimales(){
     return this.http.get<Animal[]>(this.UrlListar);
+  }
+  agregarAnimal(){
+    return this.http.post<Animal[]>(this.UrlGuardar, animal);
   }
 }
